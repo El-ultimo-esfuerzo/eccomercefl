5ce47d04eb51e901adfac9a73c8d7a77fc11adf4
frayluis11 <107652764+frayluis11@users.noreply.github.com>
2024-12-01 18:43:20 -0500

    Primer commit: inicialización del proyecto

---
+++ eccomercefl/src/main/java/com/elultimosefuerzo/eccomercefl/EccomerceflApplication.java
@@ -0,0 +1,13 @@
+package com.elultimosefuerzo.eccomercefl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccomerceflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccomerceflApplication.class, args);
+	}
+
+}
---
+++ eccomercefl/src/test/java/com/elultimosefuerzo/eccomercefl/EccomerceflApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elultimosefuerzo.eccomercefl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccomerceflApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
